3d0da51fa87150a7a27a82e01a38466fe633e1d7
Amanda Ross <amikaross@gmail.com>
2023-07-05 16:29:57 -0600

    Add oz's plan of encoding length of string in the second byte. TBD - decoding :P)

--- sneaky_nan.py
+++ sneaky_nan.py
@@ -4,19 +4,15 @@ def conceal(message):
     if len(message) > 6:
         return "Sorry, your message is too long."
 
-    byte_array = [0b11111111, 0b11111111]
+    byte_array = [0xff, 248 + len(message)]
     byte_array += bytes(message, 'utf-8')
     for _ in range(8 - len(byte_array)):
         byte_array.append(32)
 
-    x = struct.unpack('>d', bytes(byte_array))[0]
-
-    return x
-
+    return struct.unpack('>d', bytes(byte_array))[0]
 
 def extract(nan):
-    full_byte_string = struct.pack('>d', nan)
-    byte_string = full_byte_string[2:]
+    byte_string = struct.pack('>d', nan)[2:]
     message = byte_string.decode('utf-8')
 
     return message.strip()
