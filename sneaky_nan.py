557b4672e415d74bb3dfc9d913c4eb49efa78c28
Amanda Ross <amikaross@gmail.com>
2023-07-05 13:27:58 -0600

    sneaky_nan first iteration

---
+++ sneaky_nan.py
@@ -0,0 +1,19 @@
+import struct 
+
+def conceal(message):
+    if len(message) > 6:
+        return "Sorry, your message is too long."
+
+    byte_array = [0b11111111, 0b11111111]
+    byte_array += bytes(message, 'utf-8')
+    x = struct.unpack('>d', bytes(byte_array))[0]
+
+    return x
+
+
+def extract(nan):
+    full_byte_string = struct.pack('>d', nan)
+    byte_string = full_byte_string[2:]
+    message = byte_string.decode('utf-8')
+
+    return message
