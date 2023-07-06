73230938dd7fbd7d448dd77304dbb91d63f17848
Amanda Ross <amikaross@gmail.com>
2023-07-06 11:33:26 -0600

    Second iteration with Oz's length trick instead of padding with whitespace and stripping

--- sneaky_nan.py
+++ sneaky_nan.py
@@ -1,18 +1,30 @@
 import struct 
 
 def conceal(message):
-    if len(message) > 6:
+    length = len(message)
+    if length > 6:
         return "Sorry, your message is too long."
 
-    byte_array = [0xff, 248 + len(message)]
+    byte_array = [0xff, 248 + length]
+    byte_array += b'\x00' * (6 - length)
     byte_array += bytes(message, 'utf-8')
-    for _ in range(8 - len(byte_array)):
-        byte_array.append(32)
 
     return struct.unpack('>d', bytes(byte_array))[0]
 
 def extract(nan):
-    byte_string = struct.pack('>d', nan)[2:]
-    message = byte_string.decode('utf-8')
+    byte_string = struct.pack('>d', nan)
+    length = byte_string[1] & 0x7
+    string = byte_string[-length:]
 
-    return message.strip()
+    return string.decode('utf-8')
+
+
+
+
+if __name__ == '__main__':
+    cases = ('secret', 'test', 'what!')
+    for case in cases:
+        x = conceal(case)
+        assert isinstance(x, float)
+        assert repr(x) == 'nan'
+        assert extract(x) == case
