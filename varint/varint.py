303ca7e5cf105ff72d610326fb6fec3762f2b71f
Amanda Ross <amikaross@gmail.com>
2023-07-16 08:15:42 -0600

    Second iteration of decoder which uses bitwise operations instead of string manip

--- varint/varint.py
+++ varint/varint.py
@@ -3,23 +3,19 @@ import struct
 def encode(integer):
   new_bits = []
   while integer > 0:
-    current_byte = integer & 0x7f
+    current_chunk = integer & 0x7f
     integer >>= 7
     if integer > 0: 
-      current_byte |= 0x80 
-    new_bits.append(current_byte)
+      current_chunk|= 0x80 
+    new_bits.append(current_chunk)
   return bytes(new_bits)
 
 def decode(bytes):
-  if bytes == b'':
-    return 0
-  bits = []
-  byte_array = bytearray(bytes)
-  byte_array.reverse()
-  for byte in byte_array:
-     dropped = byte & 0x7f
-     bits.append(bin(dropped)[2:].zfill(7))
-  return int("".join(bits), 2)
+  num = 0
+  for byte in reversed(bytes):
+    num <<= 7
+    num |= (byte & 0x7f)
+  return num
 
 def read_int_file(filename):
    f = open(filename, 'rb')
@@ -31,6 +27,8 @@ if __name__ == '__main__':
 
     assert encode(150) == b'\x96\x01'
     assert decode(b'\x96\x01') == 150 
+    assert encode(18446744073709551615) == b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\x01'
+    assert decode(b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\x01') == 18446744073709551615
 
     assert decode(encode(read_int_file("1.uint64")))
     assert decode(encode(read_int_file("150.uint64")))
